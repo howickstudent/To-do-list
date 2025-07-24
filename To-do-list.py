cca549bde9d5c0538a56a26627b7f953514b4601
howickstudent <st24663@howick.school.nz>
2025-07-25 09:57:26 +1200

    Improved login and register
    Login verification and registering users is now pretty much complete. Small improvements like a back button in the registration frame.

--- To-do-list.py
+++ To-do-list.py
@@ -58,10 +58,10 @@ class ToDoList():
         self.loginText.place(x=204, y=300)
         return loginFrame
     
-    def LoginVerif(self, username, password):
+    def LoginVerif(self, username, password):       # login verification
         success = 0
         try:
-            with open("RegisteredUsers.txt", "r") as file:
+            with open("RegisteredUsers.txt", "r") as file:  # it will read the file and once it finds the username and corresponding password, it will successfully log them in and change the login button to continure where it will lead them to the to do list program.
                 for i in file:
                     fileuser, filepassword = i.strip().split(" ")
                     if username == fileuser and password == filepassword:
@@ -73,12 +73,12 @@ class ToDoList():
                     self.loginText.config(text="User not found.", fg="red")
         except FileNotFoundError:
             with open("RegisteredUsers.txt", "w") as file:
-                print(file)
+                file.write("John Doe")
 
     def RegisFrame(self):                 # the login frame which will be shown when they exit the menu frame after pressing the login button.
         regisFrame = Frame(self.container)
         regisFrame.grid(row=0, column=0, sticky="nsew", ipadx=300, ipady=600)
-        
+                                                                                            # it is basically the same as the login menu with the difference being the register button and there will also be a back button when they have finished registering.
         regisLabel = Label(regisFrame, text="Register", font=("Corbel", 50, "bold"), wraplength=600)
         regisLabel.place(x= 120, y=130)
 
@@ -94,13 +94,25 @@ class ToDoList():
         regisEntryPassword = Entry(regisFrame)
         regisEntryPassword.place(x=185, y=280)
 
-        regisButton = Button(regisFrame, text="Register", width=10, command=lambda:self.LoginVerif(regisEntryUser.get(), regisEntryPassword.get()))
+        backButton = Button(regisFrame, text="Back", command=lambda:self.ShowFrame("LoginFrame")) # if the back button is pressed it will call the showframe function and show the login frame.
+        backButton.place(x=300, y=320)
+
+        regisButton = Button(regisFrame, text="Register", width=10, command=lambda:self.Register(regisEntryUser.get(), regisEntryPassword.get()))  # when the register button has been clicked it will run the register function however the register function will need the username and password as parameters so i will use .get() to obtain it from the entry boxes.
         regisButton.place(x=206, y=320)
         return regisFrame
     
-    # def Register(self, username, password):
-    #     with open("RegisteredUsers.txt", "a") as file:
-
+    def Register(self, username, password):         # register system
+        try:
+            with open("RegisteredUsers.txt", "r") as fileread:  # reading mode in order to check its content
+                for i in fileread:
+                    fileuser, filepassword = i.strip().split(" ")   # separating the username and the password
+                    if username == fileuser and password == filepassword:
+                        print("account found")                  # if the username and password have already been registered to the txt file, it will tell them that the account has been found and it will not be adding the username and password to prevent duplicates
+                with open("RegisteredUsers.txt", "a") as fileadd:
+                    fileadd.write("\n" + username + " " + password) # if it is confirmed that the username and password are not yet added it will then add it to the txt file 
+        except FileNotFoundError:
+            with open("RegisteredUsers.txt", "w") as file:
+                file.write("John Doe")
 
 # main program
 root = Tk()
